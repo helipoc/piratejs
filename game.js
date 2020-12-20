b340e4d8518167cc9687a32aad8359da78a2f4f0
helipoc <klonja0x@gmail.com>
2020-12-20 21:35:29 +0100

    refactoring

--- game.js
+++ game.js
@@ -1,6 +1,4 @@
 
-var coord={ex:0,px:0}
-
 var player;
 
 window.onload=()=>{
@@ -13,7 +11,6 @@ document.addEventListener("click",(e)=>{
     player.style.top=0+"px";
     let offX=e.offsetX;
     let offY=e.offsetY;
-    coord["px"]=e.offsetX;
     player.style.left=offX+"px";
     player.style.top=offY+"px";
     if(!aliveEnemy)Spawn();
@@ -23,11 +20,12 @@ document.addEventListener("click",(e)=>{
 
 
 
-var enemy;
-var redX;
+var enemy; // enemy
+var redX; // interval each 1s pick a new random position for the enemy and delete the interval when the specific enemy dies
 let aliveEnemy=false;
 function Spawn(){
 aliveEnemy=true;
+enemyFire.hidden=false;
 enemy = document.getElementById("enemy")
 enemy.getElementsByTagName('img')[0].hidden=false;
 enemy.style.opacity=1;
@@ -38,9 +36,9 @@ enemy.getElementsByTagName("progress")[0].hidden=false;
 
 redX = setInterval(() => {
 let rnx = Math.random()*window.innerWidth;
+let rny = Math.random()*window.innerHeight;
 enemy.style.left=Math.floor(rnx)+"px";
-enemy.style.top=Math.floor(Math.random()*window.innerHeight)+"px";
-coord["ex"]=Math.ceil(rnx);
+enemy.style.top=Math.floor(rny)+"px";
 if(enemy.health<=0){
     enemy.style.transition="all 0s linear";
     enemy.style.opacity=0;  
@@ -60,19 +58,35 @@ if(enemy.health<=0){
 
 
 
-
+//listen on "space" click event to fire !
 document.onkeypress = (e) => {
     if(e.code=="Space"){
-        if(Math.abs(coord["ex"]-coord["px"])<120){
-        enemy.health-=player.ad;
-        healthbarEnemy.value-=player.ad;
-        info.textContent=`Score : ${player.score}, Health : ${player.health}`
+        if(Math.abs(enemyTarget.getBoundingClientRect().x-playerTarget.getBoundingClientRect().x)<120){
+            info.textContent=`Score : ${player.score}, Health : ${player.health}`
+            enemy.health-=player.ad;
+            healthbarEnemy.value-=player.ad;
+        if(enemyTarget.getBoundingClientRect().y>playerTarget.getBoundingClientRect().y){
+            playerFire.style.top="1000px";
+        }else {
+            playerFire.style.top="-1000px";
         }
+      
+        
+        setTimeout(()=>{
+            playerFire.style.left=0;
+            playerFire.style.top="-15px";
+        },800)
+        }
+        
     }
+   
 }
 
 
 
+
+
+//interval for enemy fire each 0.5sec if enemy and player are in the same X level
 setInterval(()=>{
     if(player.health<=0){
     
@@ -80,10 +94,23 @@ setInterval(()=>{
         alert("You lost :(")
 
     }
-    if(Math.abs(coord["ex"]-coord["px"])<120 && aliveEnemy){
+    if(Math.abs(enemyTarget.getBoundingClientRect().x-playerTarget.getBoundingClientRect().x)<80 && aliveEnemy){
         player.health-=10;
         healthbarPlayer.value-=10;
         info.textContent=`Score : ${player.score}, Health : ${player.health}`
+        if(enemyTarget.getBoundingClientRect().y<playerTarget.getBoundingClientRect().y){
+            enemyFire.style.top="1000px";
+            
+        }
+        else {
+            enemyFire.style.top="-1000px";
+        }  
+        setTimeout(()=>{
+            enemyFire.style.left=0;
+            enemyFire.style.top="-15px";
+        },800)
+
+        
     } 
 },500)
 
